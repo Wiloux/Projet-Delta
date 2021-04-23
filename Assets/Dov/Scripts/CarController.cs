df5451255b0898d563c08527189ad2070585def2
Dov <dovbenzak2@hotmail.com>
2021-04-23 15:46:17 +0200

    [FEAT] Import kart & car controller

---
+++ Assets/Dov/Scripts/CarController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float _horizontalInput;
+    private float _verticalInput;
+    private float _currentsteerAngle;
+    private float _currentbreakForce;
+    private bool _isBreaking;
+
+    [SerializeField] private float _motorForce;
+    [SerializeField] private float _breakForce;
+    [SerializeField] private float _maxSteerAngle;
+
+    [SerializeField] private WheelCollider _frontLeftWheelCollider;
+    [SerializeField] private WheelCollider _frontRightWheelCollider;
+    [SerializeField] private WheelCollider _rearLeftWheelCollider;
+    [SerializeField] private WheelCollider _rearRightWheelCollider;
+
+    [SerializeField] private Transform _frontLeftWheelTransform;
+    [SerializeField] private Transform _frontRightWheelTransform;
+    [SerializeField] private Transform _rearLeftWheelTransform;
+    [SerializeField] private Transform _rearRightWheelTransform;
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+
+    }
+    private void GetInput()
+    {
+        _horizontalInput = Input.GetAxis(HORIZONTAL);
+        _verticalInput = Input.GetAxis(VERTICAL);
+        _isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        _frontLeftWheelCollider.motorTorque = _verticalInput * _motorForce;
+        _frontRightWheelCollider.motorTorque = _verticalInput * _motorForce;
+        _currentbreakForce = _isBreaking ? _breakForce : 0f;
+        if (_isBreaking)
+        {
+            ApplyBreaking();
+        }
+    }
+
+    private void ApplyBreaking()
+    {
+        _frontLeftWheelCollider.brakeTorque = _currentbreakForce;
+        _frontRightWheelCollider.brakeTorque = _currentbreakForce;
+        _rearLeftWheelCollider.brakeTorque = _currentbreakForce;
+        _rearRightWheelCollider.brakeTorque = _currentbreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        _currentsteerAngle = _maxSteerAngle * _horizontalInput;
+        _frontLeftWheelCollider.steerAngle = _currentsteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(_frontLeftWheelCollider, _frontLeftWheelTransform);
+        UpdateSingleWheel(_frontRightWheelCollider, _frontRightWheelTransform);
+        UpdateSingleWheel(_rearLeftWheelCollider, _rearLeftWheelTransform);
+        UpdateSingleWheel(_rearRightWheelCollider, _rearRightWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
---
+++ Assets/Dov/Scripts/MountKartingController.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MountKartingController : MonoBehaviour
+{
+    public Transform kartModel;
+    public Transform kartNormal;
+    public Rigidbody sphere;
+
+    private float speed;
+    private float currentSpeed;
+    private float rotate;
+    private float currentRotate;
+
+    private int driftDirection;
+    private int driftMode = 0;
+
+    [Header("Bools")]
+    public bool drifting;
+
+    [Header("Boost")]
+    public bool raphquidecide;
+
+    [Header("Parameters")]
+    public float acceleration = 30f;
+    public float steering = 80f;
+    public float gravity = 10f;
+    public LayerMask layerMask;
+
+    [Header("Model Parts")]
+    public Transform frontWheels;
+    public Transform backWheels;
+    public Transform steeringWheel;
+
+
+    private void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float time = Time.timeScale == 1 ? .2f : 1;
+            Time.timeScale = time;
+        }*/
+
+        //Follow Collider
+        transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+
+        //Accelerate
+        if (Input.GetButton("Fire1"))
+            speed = acceleration;
+
+        if (Input.GetButton("Fire2"))
+            speed -= acceleration * 0.2f;
+
+        //Steer
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            Steer(dir, amount);
+        }
+
+        //Drift
+        if (Input.GetButtonDown("Jump") && !drifting && Input.GetAxis("Horizontal") != 0)
+        {
+            drifting = true;
+            driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+
+            //kartModel.parent.DOComplete();
+            //kartModel.parent.DOPunchPosition(transform.up * .2f, .3f, 5, 1);
+        }
+
+        if (drifting)
+        {
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            Steer(driftDirection, control);
+        }
+
+        if (Input.GetButtonUp("Jump") && drifting)
+        {
+            if(raphquidecide)
+                Boost();
+        }
+
+        currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
+        currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+
+
+        if (!drifting)
+            kartModel.localEulerAngles = Vector3.Lerp(kartModel.localEulerAngles, new Vector3(0, 90 + (Input.GetAxis("Horizontal") * 15), kartModel.localEulerAngles.z), .2f);
+        else
+        {
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, .5f, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, .5f);
+            kartModel.parent.localRotation = Quaternion.Euler(0, Mathf.LerpAngle(kartModel.parent.localEulerAngles.y,(control * 15) * driftDirection, .2f), 0);
+        }
+
+        frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * 15), frontWheels.localEulerAngles.z);
+        frontWheels.localEulerAngles += new Vector3(0, 0, sphere.velocity.magnitude / 2);
+        backWheels.localEulerAngles += new Vector3(0, 0, sphere.velocity.magnitude / 2);
+
+        steeringWheel.localEulerAngles = new Vector3(-25, 90, ((Input.GetAxis("Horizontal") * 45)));
+    }
+
+    private void FixedUpdate()
+    {
+        //Forward Acceleration
+        if(!drifting)
+            sphere.AddForce(-kartModel.transform.right * currentSpeed, ForceMode.Acceleration);
+        else
+            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+
+        //Gravity
+        sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        //Steering
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+        //RaycastHit hitOn;
+        RaycastHit hitNear;
+
+        //Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitOn, 1.1f, layerMask);
+        Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitNear, 2.0f, layerMask);
+
+        //Normal Rotation
+        kartNormal.up = Vector3.Lerp(kartNormal.up, hitNear.normal, Time.deltaTime * 8.0f);
+        kartNormal.Rotate(0, transform.eulerAngles.y, 0);
+    }
+
+    public void Boost()
+    {
+        drifting = false;
+
+        if (driftMode > 0)
+        {
+            DOVirtual.Float(currentSpeed * 3, currentSpeed, .3f * driftMode, Speed);
+        }
+
+        driftMode = 0;
+        kartModel.parent.DOLocalRotate(Vector3.zero, .5f).SetEase(Ease.OutBack);
+    }
+
+    public void Steer(int direction, float amount)
+    {
+        rotate = (steering * direction) * amount;
+    }
+
+    private void Speed(float x)
+    {
+        currentSpeed = x;
+    }
+}
