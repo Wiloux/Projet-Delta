50a783602db35050535283d6f01bb95fafd9c38f
Wiloux <54811449+Wiloux@users.noreply.github.com>
2021-05-21 15:21:06 +0200

    Revert "Revert "Merge branch 'main' into Wiloux""
    This reverts commit 1525f15776b11eec47ffc0893f3b591767957e38.

--- Assets/Florian/Script/JumpingSheep.cs
+++ Assets/Florian/Script/JumpingSheep.cs
@@ -7,42 +7,51 @@ namespace Florian
 {
     public class JumpingSheep : MonoBehaviour
     {
-        [SerializeField] private MovementController movement;
-        private bool _jump = false;
+        private MovementController _movementController;
 
         [Header("MegaJump")]
-        public float _megaJumpForce;
+        public float _megaJumpForce = 100f;
+        public float _megaAccelForce = 10f;
 
         [Header("Stomp")]
         public int _nbrStomp = 0;
-        public float _megaGravity;
         public float _stompRadius;
+        public float _stompForce = 50f;
+
+
+        private void Start()
+        {
+            _movementController = GetComponent<MovementController>();
+        }
 
         private void Update()
         {
-            /*if (_jump)
+            //ChangeWoolColor();
+        }
+
+        public void MegaJump()
+        {
+            _movementController.physics.AddVelocity(new Vector3(0f, _megaJumpForce, _megaAccelForce));
+        }
+
+        public void Stomp()
+        {
+            _movementController.physics.AddVelocity(transform.worldToLocalMatrix.MultiplyVector(-transform.up) * _stompForce);
+
+            Collider[] colliders = Physics.OverlapSphere(-transform.up + transform.localPosition, _stompRadius);
+
+            foreach (Collider pushedObject in colliders)
             {
-                if (movement.airborn)
+                if (pushedObject.CompareTag("Player") && pushedObject.name != gameObject.name)
                 {
-                    MegaJump();
-                }
-                else
-                {
-                    Stomp();
+                    MovementController movementController = pushedObject.GetComponent<MovementController>();
+                    movementController.physics.AddVelocity(
+                        pushedObject.transform.worldToLocalMatrix.MultiplyVector(-transform.forward) * _stompForce
+                    );
+                    movementController.physics.TimedChange(ref movementController.physics.frictions.amplitude, "frictions.amplitude", movementController.physics.frictions.amplitude * 15f, 1f);
+                    movementController.physics.TimedChange(ref movementController.physics.stun, "stun", true, 2.5f);
                 }
             }
-            ChangeWoolColor();*/
-        }
-
-        private void MegaJump()
-        {
-            movement.physics.jumpForce = _megaJumpForce;
-        }
-
-        private void Stomp()
-        {
-            _nbrStomp++;
-            //movement.gravity = _megaGravity;
 
         }
 
@@ -57,7 +66,7 @@ namespace Florian
         public void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawSphere(transform.position, 5f);
+            Gizmos.DrawSphere(-transform.up + transform.localPosition, 5f);
         }
     }
 }
