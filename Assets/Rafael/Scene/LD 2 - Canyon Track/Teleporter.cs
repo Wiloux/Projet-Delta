2a956d9659397d413c55700c657392b288f0639a
Wiloux <54811449+Wiloux@users.noreply.github.com>
2021-06-04 15:23:16 +0200

    updated teleporters

--- Assets/Rafael/Scene/LD 2 - Canyon Track/Teleporter.cs
+++ Assets/Rafael/Scene/LD 2 - Canyon Track/Teleporter.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
 
-    public Transform[] exits;
+    public List<Transform> exits = new List<Transform>();
+
+    public List<string> playerNames = new List<string>();
+
+    public bool original;
 
     //Gizmos
     private BoxCollider col;
@@ -19,30 +23,50 @@ public class Teleporter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && exits.Length > 0)
+        if (other.CompareTag("Player") && exits.Count > 0 && original)
         {
-            int index = Random.Range(0, exits.Length);
-            Debug.Log(exits.Length);
+
+            int index = Random.Range(0, exits.Count);
+            foreach (Transform exit in exits)
+            {
+                if (index != exits.IndexOf(exit))
+                {
+                    exit.parent.GetComponent<Teleporter>().playerNames.Add(other.transform.name);
+                    break;
+                }
+            }
 
             other.transform.position = exits[index].position;
             other.transform.rotation = exits[index].rotation;
-            Debug.Log("Player : " + other.transform.position + " ; Exit : " + exits[index].position);
-
         }
-    }
-
-
-    private void OnDrawGizmos()
-    {
-
-        if (col && child)
+        else if (!original && other.CompareTag("Player") && exits.Count > 0)
         {
+            for (int i = 0; i < playerNames.Count; i++)
+            {
+                string n = playerNames[i];
+                if (other.transform.name == n)
+                {
+                    int index = Random.Range(0, exits.Count);
+                    if (exits[index].parent.GetComponent<Teleporter>().exits.Count != 0)
+                    {
+                        foreach (Transform exit in exits)
+                        {
+                            if (index != exits.IndexOf(exit))
+                            {
+                                exit.parent.GetComponent<Teleporter>().playerNames.Add(other.transform.name);
+                                break;
+                            }
+                        }
+                    }
 
-            //Gizmos.matrix = this.transform.localToWorldMatrix;
-            Gizmos.DrawCube(transform.position, col.size);
 
-            Gizmos.DrawSphere(child.position, 1);
+                    playerNames.Remove(n);
+                    other.transform.position = exits[index].position;
+                    other.transform.rotation = exits[index].rotation;
+                }
+
+            }
+
         }
-
     }
 }
