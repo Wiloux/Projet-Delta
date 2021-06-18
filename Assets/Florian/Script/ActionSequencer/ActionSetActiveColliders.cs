7818f9de0ca651973d8fda843b0f0fce069d067b
Laefar <laefar@outlook.fr>
2021-06-18 17:29:07 +0200

    [INT] Enviro + Collision + Shortcut 1 + Scene fusion

--- Assets/Florian/Script/ActionSequencer/ActionSetActiveColliders.cs
+++ Assets/Florian/Script/ActionSequencer/ActionSetActiveColliders.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 namespace Florian.ActionSequencer {
     public class ActionSetActiveColliders : Action {
-        public GameObject colliderObject;
+        public Collider colliderObject;
         public bool active = false;
 
         protected override void OnStart() {  }
 
         protected override void OnExecute() {
-            Collider[] colliders = gameObject.GetComponents<Collider>();
-            if(colliders == null || colliders.Length <= 0) { return; }
+
+            colliderObject.enabled = active;
+
+
+            /*Collider[] colliders = gameObject.GetComponents<Collider>();
+            if(colliders == null || colliders.Length <= 0) { Debug.Log(colliders.Length); return; }
 
             for (int i = 0; i < colliders.Length; i++) {
                 colliders[i].enabled = active;
-            }
+            }*/
         }
 
         public override bool IsActionEnded() {
